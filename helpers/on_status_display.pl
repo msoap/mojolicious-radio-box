8b208ce8083e800a09704307254716984402cbda
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-01-25 23:59:15 +0200

    added helper script for cmus, for save track title of internet-radio stream

---
+++ helpers/on_status_display.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+=head1 DESCRIPTION
+
+Place this script into ~/.cmus/ for save title for last track or title of icecast stream
+and change ~/.cmus/autosave:
+    set status_display_program=~/.cmus/on_status_display.pl
+
+=cut
+
+use strict;
+use warnings;
+
+use JSON;
+
+# ------------------------------------------------------------------------------
+sub main {
+    my %params = @ARGV;
+
+    open my $FH, '>', "$ENV{HOME}/.cmus/last_track.txt" or die "Error open file: $!\n";
+    print $FH to_json(\%params) . "\n";
+    close $FH;
+}
+
+# ------------------------------------------------------------------------------
+main();
